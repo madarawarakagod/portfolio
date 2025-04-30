d98d69d04f112ec088ec21090dc901802878e0d6
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-04-30 10:04:04 +0530

    update home page
    set background,create icon for color mode,create mobile menu

--- myportfolio/components/Navbar.jsx
+++ myportfolio/components/Navbar.jsx
@@ -6,6 +6,10 @@ import React from 'react'
 function Navbar() {
   return (
     <>
+ 
+      <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+      <Image src={assets.header_bg_color} alt='' className='w-full'/>
+    </div>
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
         <a href="#top">
             <Image src={assets.logo} className='w-35 cursor-pointer mr-14' alt="" />
@@ -17,9 +21,33 @@ function Navbar() {
             <li><a href="#work">My Work</a></li>
             <li><a href="#contact">Contact me</a></li>
         </ul>
-        <div>
-            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-grey-500 rounded-full ml-4'>Contact<Image src={assets.arrow_icon} alt="" className='w-3'/> </a>
+        <div className='flex items-center gap-4'>
+          <button>
+            <Image src={assets.moon_icon}alt=' ' className='w-6'/>
+          </button>
+
+                   <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-grey-500 rounded-full ml-4'>
+                    Contact<Image src={assets.arrow_icon} alt="" className='w-3'/> </a>
+
+                    <button className='block md:hidden ml-3'>
+                      <Image src={assets.menu_black} alt='' className='w-6'/>
+
+                    </button>
+
         </div>
+
+        {/*mobile menu*/}
+        <ul className='flex md:hidden flex-col gap-4 py-20
+         px-10 fixed-right-0 top-0 bottom-0 w-64 z-50 h-screen
+          bg-rose-50 transsition duration-500'>
+   
+        
+            <li><a href="#top">Home</a></li>
+            <li><a href="#about">About me</a></li>
+            <li><a href="#service">Services</a></li>
+            <li><a href="#work">My Work</a></li>
+            <li><a href="#contact">Contact me</a></li>
+        </ul>
     </nav>
     </>
   )
