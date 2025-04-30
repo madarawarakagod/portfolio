fe79b2a5b71a12aba8826fa69fc08227c070ce4f
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-04-30 20:09:18 +0530

    update open menu

--- myportfolio/components/Navbar.jsx
+++ myportfolio/components/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
                    <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-grey-500 rounded-full ml-4'>
                     Contact<Image src={assets.arrow_icon} alt="" className='w-3'/> </a>
 
-                    <button className='block md:hidden ml-3'>
+                    <button className='block md:hidden ml-3' onClick={openMenu}>
                       <Image src={assets.menu_black} alt='' className='w-6'/>
 
                     </button>
