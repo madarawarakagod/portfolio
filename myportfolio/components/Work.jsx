de1c2ee99aeafa40a06b7dfd5f76e06960ea786f
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-02 00:03:18 +0530

    create work and add imgaes

--- myportfolio/components/Work.jsx
+++ myportfolio/components/Work.jsx
@@ -9,23 +9,33 @@ const Work = () => {
 <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
         <h4 className='text-center mb-2 text-lg font-Ovo'>my portfolio</h4>
         <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
+
+
         <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-            I am Full-Stack developer from Sri Lanka,and Undergratuated Student in University of Jaffna.</p>
+            I am Full-Stack developer from Sri 
+            Lanka,and Undergratuated Student in 
+            University of Jaffna.</p>
 
-            <div className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-              {workData.map((project,index)=>(
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-10">
+                      {workData.map((project,index)=>(
                 <div key={index} 
+                   className='aspect-square bg-no-repeat bg-cover bg-center
+                    rounded-lg relative cursor-pointer group'
+                    style={{backgroundImage: `url(${project.bgImage})`}}>
 
-                   className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group
-                  'style={{backgroundImage: `url(${project.bgImage})`}}>
+
                     <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex
                      items-center justify-between duration-500 group-hover:bottom-7'>
-                      <div>
+
+
+
+                    <div>
                     <h2 className='font-semibold'>{project.title}</h2>
                     <p className='text-sm text-gray-700'>{project.description}</p>
                   </div>
 
-                <div>
+                <div className='border rounded-full border-black w-9 aspect-square flex items-center
+                 justify-center shodow-[2px_2px_0_#000]  group-hover:bg-lime-300 transition'>
                 <Image src={assets.send_icon} alt='send_icon' className='w-5'/>
                 </div>
                 </div>
