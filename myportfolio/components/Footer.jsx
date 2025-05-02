08efea63959598cd34fec6a585b985bbdce98a99
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-02 11:23:13 +0530

    create footer

---
+++ myportfolio/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+        <div className='text-center'>
+            <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+
+            <div className='w-max flex items-center gap-2 mx-auto'>
+
+                <Image src={assets.mail_icon} alt='' className='w-6'/>
+                madarawarakagoda8@gmail.com
+            </div>
+        </div>
+        <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'> 
+            <p>© 2025 Madara Warakagoda. All rights reserved.</p> 
+       <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'> 
+        <li><a target='_blank' href="https://github.com/madarawarakagod">GitHub</a></li> 
+
+      <li><a target='_blank' href="https://www.linkedin.com/in/madara-warakagoda-283999281/">LinkedIn</a></li> 
+      <li><a target='_blank' href="https://medium.com/@madarawarakagoda8">Medium</a></li> 
+</ul> 
+</div> 
+    </div>
+    
+  )
+}
+
+export default Footer
